3343742c0a0e35532905d7cc29b0e9a00754d27b
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2021-01-21 12:25:14 +0100

    Add UserEditScreen

---
+++ frontend/src/screens/UserEditScreen.tsx
@@ -0,0 +1,121 @@
+import React, { FC, useState, useEffect, FormEvent } from 'react'
+import { Link, RouteComponentProps } from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import BaseMessage from '../components/UI/BaseMessage'
+import Loader from '../components/UI/Loader'
+import FormContainer from '../components/UI/FormContainer'
+import { getUserDetails, updateUser } from '../redux/actions/userActions'
+import {
+  IUserDetailsState,
+  IUserUpdateState,
+  UserActions,
+  UserActionTypes
+} from '../redux/types/userTypes'
+import { IApplicationState } from '../redux/reducers/rootReducer'
+import { ThunkDispatch } from 'redux-thunk'
+
+const UserEditScreen: FC<RouteComponentProps<{ id: string }>> = ({
+  match,
+  history
+}) => {
+  const userId = match.params.id
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [isAdmin, setIsAdmin] = useState(false)
+
+  const dispatch: ThunkDispatch<
+    IApplicationState,
+    any,
+    UserActions
+  > = useDispatch()
+
+  const userDetails = useSelector<IApplicationState, IUserDetailsState>(
+    (state) => state.userDetails
+  )
+  const { loading, error, user } = userDetails
+
+  const userUpdate = useSelector<IApplicationState, IUserUpdateState>(
+    (state) => state.userUpdate
+  )
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate
+  } = userUpdate
+
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: UserActionTypes.USER_UPDATE_RESET })
+      history.push('/admin/userList')
+    } else {
+      if (!user || !user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+        setIsAdmin(user.isAdmin)
+      }
+    }
+  }, [dispatch, history, userId, user, successUpdate])
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }))
+  }
+
+  return (
+    <>
+      <Link to='/admin/userList' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && (
+          <BaseMessage variant='danger'>{errorUpdate}</BaseMessage>
+        )}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <BaseMessage variant='danger'>{error}</BaseMessage>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Enter name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='isAdmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+            <Button type='submit' variant='primary'>
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  )
+}
+
+export default UserEditScreen
